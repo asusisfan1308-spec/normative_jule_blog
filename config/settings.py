3aa89de616d68b1abcfeab21118f4424c4ef1589
Isfandiyor <isfan13082010@example.com>
2026-09-13 22:13:32 +0500

    email final

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'asus.isfan.1308@gmail.com')
-EMAIL_HOST_PASSWORD = 'fmqz lndf nlak szoi'
+EMAIL_HOST_USER = os.getenv('asus.isfan.1308@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('fmqz lndf nlak szoi')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
