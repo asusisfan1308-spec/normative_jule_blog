2897d01a5cc1ab27c3994103d629b33c9b3b85d0
Isfandiyor <isfan13082010@example.com>
2026-09-13 21:06:16 +0500

    email

--- config/settings.py
+++ config/settings.py
@@ -115,8 +115,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'post_list'
 
 LOGOUT_REDIRECT_URL = 'post_list'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'asus.isfan.1308@gmail.com'
+EMAIL_HOST_PASSWORD = 'isfan_1308'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
