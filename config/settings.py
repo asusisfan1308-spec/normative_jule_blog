137e2da9c1a41bf23443a481f21ec2e2868afc54
Isfandiyor <isfan13082010@example.com>
2026-09-14 15:15:45 +0500

    emaill

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
